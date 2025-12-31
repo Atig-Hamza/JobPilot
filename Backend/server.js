7b7b9a36e27a3a0ce9febb727a7c2e05e5907ed2
Atig-Hamza <hamzaatig@icloud.com>
2026-01-01 00:20:30 +0100

    feat: set up database connection and configure middleware in server

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./src/config/database');
 const routes = require('./src/routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
