3eec5d0919d9e25947e077de073e93dc36c65490
Atig-Hamza <hamzaatig@icloud.com>
2025-12-31 15:02:18 +0100

    feat: add route for Terms and Privacy page to enhance navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import './App.css';
+import TermsAndPrivacy from './pages/TermsAndPrivacy';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/privacy" element={<TermsAndPrivacy />} />
       </Routes>
     </Router>
   );
