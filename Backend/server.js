2814a3873040796d238ed865d3033b414c18afdc
Atig-Hamza <hamzaatig@icloud.com>
2026-01-09 23:38:47 +0000

    feat: bind server to 0.0.0.0 for external access

--- Backend/server.js
+++ Backend/server.js
@@ -23,6 +23,6 @@ app.all('*', (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
