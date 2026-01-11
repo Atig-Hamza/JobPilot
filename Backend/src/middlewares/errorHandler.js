64e4bce75597ed7e52e024fa92b4e75b78e9d825
Atig-Hamza <hamzaatig@icloud.com>
2026-01-11 15:09:55 +0000

    feat: enhance error handling with specific handlers for CastError, DuplicateFields, and ValidationError

--- Backend/src/middlewares/errorHandler.js
+++ Backend/src/middlewares/errorHandler.js
@@ -1,3 +1,22 @@
+import { AppError } from '../utils/AppError.js';
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg ? err.errmsg.match(/(["'])(\\?.)*?\1/)[0] : Object.values(err.keyValue)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -10,16 +29,24 @@ const errorHandler = (err, req, res, next) => {
       stack: err.stack
     });
   } else {
-    if (err.isOperational) {
-      res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
+    let error = { ...err };
+    error.message = err.message;
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+
+    if (error.isOperational) {
+      res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message
       });
     } else {
       console.error('ERROR 💥', err);
       res.status(500).json({
         status: 'error',
-        message: 'Something went very wrong!'
+        message: 'Something went very wrong!',
+        devError: err.message
       });
     }
   }
