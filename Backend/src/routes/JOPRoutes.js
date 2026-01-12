6de0d5a80f81a53c35fd0cc567b021793d621690
Atig-Hamza <hamzaatig@icloud.com>
2026-01-12 14:53:00 +0000

    feat: add authentication middleware to crawl route for protection

--- Backend/src/routes/JOPRoutes.js
+++ Backend/src/routes/JOPRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { crawlCompanies } from '../controllers/JOPController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/crawl', crawlCompanies);
+router.post('/crawl', protect, crawlCompanies);
 
 export default router;
