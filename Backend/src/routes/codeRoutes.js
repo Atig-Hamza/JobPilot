4b59e448b2be2b41b1e9a27021cb2935f455d0fc
Atig-Hamza <hamzaatig@icloud.com>
2026-01-16 09:55:13 +0000

    feat: update codeRoutes to include admin protection for invite and credit code management

--- Backend/src/routes/codeRoutes.js
+++ Backend/src/routes/codeRoutes.js
@@ -1,9 +1,12 @@
-import { createInviteCode, createCreditCode } from "../controllers/codeController.js";
+import { createInviteCode, createCreditCode, getInviteCodes, deleteInviteCode } from "../controllers/codeController.js";
 import { Router } from "express";
+import { protect, restrictTo } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/invite", createInviteCode);
-router.post("/credit", createCreditCode);
+router.post("/create-invite", protect, restrictTo('admin'), createInviteCode);
+router.get("/invite-codes", protect, restrictTo('admin'), getInviteCodes);
+router.delete("/invite-codes/:id", protect, restrictTo('admin'), deleteInviteCode);
+router.post("/create-credit", protect, restrictTo('admin'), createCreditCode);
 
 export default router;
