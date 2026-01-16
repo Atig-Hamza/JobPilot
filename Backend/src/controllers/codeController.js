994f02f337bebba521c8f1b0d13550e581998216
Atig-Hamza <hamzaatig@icloud.com>
2026-01-16 09:55:05 +0000

    feat: add getInviteCodes and deleteInviteCode functions to manage invite codes

--- Backend/src/controllers/codeController.js
+++ Backend/src/controllers/codeController.js
@@ -1,10 +1,29 @@
-import { useCreditCode, generateCreditCode, generateInviteCode } from "../services/codeService.js";
+import { useCreditCode, generateCreditCode, generateInviteCode, getAllInviteCodes, deleteInviteCode as deleteInviteCodeService } from "../services/codeService.js";
 
 export async function createInviteCode(req, res) {
     try {
         const { code, availableFor, expiresAt } = req.body;
         const newInviteCode = await generateInviteCode(code, availableFor, expiresAt);
         res.status(201).json(newInviteCode);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function getInviteCodes(req, res) {
+    try {
+        const inviteCodes = await getAllInviteCodes();
+        res.status(200).json(inviteCodes);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export async function deleteInviteCode(req, res) {
+    try {
+        const { id } = req.params;
+        await deleteInviteCodeService(id);
+        res.status(200).json({ message: 'Invite code deleted successfully' });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
