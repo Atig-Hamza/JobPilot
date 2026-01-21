b6a17566a3946b24ebccc661735af39b2ed163fe
Atig-Hamza <hamzaatig@icloud.com>
2026-01-21 17:05:45 +0000

    feat: add authentication middleware to user routes for password update and account deletion

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,6 +1,13 @@
 import express from 'express';
 
+import { protect } from '../middlewares/authMiddleware.js';
+import { updatePasswordController, deleteAccountController } from '../controllers/userController.js';
+
 const router = express.Router();
 
+router.use(protect);
+
+router.patch('/update-password', updatePasswordController);
+router.delete('/delete-account', deleteAccountController);
 
 export default router;
