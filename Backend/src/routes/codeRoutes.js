d6de2f47eb96cc536fdb5cc51533cb334b5ab5e8
Atig-Hamza <hamzaatig@icloud.com>
2026-02-01 00:54:09 +0000

    feat: Add routes for redeeming credits and sending credits

--- Backend/src/routes/codeRoutes.js
+++ Backend/src/routes/codeRoutes.js
@@ -1,4 +1,4 @@
-import { createInviteCode, createCreditCode, getInviteCodes, deleteInviteCode } from "../controllers/codeController.js";
+import { createInviteCode, createCreditCode, getInviteCodes, deleteInviteCode, redeemCreditCode, sendCredits } from "../controllers/codeController.js";
 import { Router } from "express";
 import { protect, restrictTo } from "../middlewares/authMiddleware.js";
 
@@ -8,5 +8,7 @@ router.post("/create-invite", protect, restrictTo('admin'), createInviteCode);
 router.get("/invite-codes", protect, restrictTo('admin'), getInviteCodes);
 router.delete("/invite-codes/:id", protect, restrictTo('admin'), deleteInviteCode);
 router.post("/create-credit", protect, restrictTo('admin'), createCreditCode);
+router.post("/redeem", protect, redeemCreditCode);
+router.post("/send-credits", protect, sendCredits);
 
 export default router;
